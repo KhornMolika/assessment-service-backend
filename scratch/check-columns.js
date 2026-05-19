95f064f74424605ba7934409eae1818a85639ecb
KhornMolika <molikakhorn71@gmail.com>
2026-05-19 14:28:15 +0700

    ignore

--- scratch/check-columns.js
+++
@@ -1,7 +0,0 @@
-const { Client } = require('pg');
-const client = new Client({ host: 'localhost', port: 5432, user: 'postgres', password: 'postgres', database: 'assessment_dev' });
-client.connect().then(async () => {
-  const res = await client.query(`SELECT column_name, is_nullable, column_default FROM information_schema.columns WHERE table_name = 'question' AND column_name = 'tags'`);
-  console.log(res.rows);
-  await client.end();
-}).catch(e => console.error(e.message));
